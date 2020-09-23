e6ab4e5e283af43f566756aed8891fddb8293499
Clayton Orman <claytonkfc@Claytons-MacBook-Pro.local>
2020-09-23 11:31:28 -0400

    ⚡ initial commit

---
+++ react.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['airbnb'],
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'react-hooks'],
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {},
+};
